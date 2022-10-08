85ccb6af466a50b0efd26c7a4c6bb8e7aeea94f7
Prakash Poudel <72375446+GitZigsaw@users.noreply.github.com>
2022-10-08 20:35:26 +0545

    GoldBach's Conjecture
    Any even number greater than 2 can be represented as a sum of two prime numbers.

---
+++ Goldbach'sConjecture.py
@@ -0,0 +1,19 @@
+# Goldbach's Conjecture states that: 
+#Any positive even integer greater than 2 can be illustrated as a sum of two prime numbers
+
+def testPrime(x):
+    c = 0
+    for i in range(2,x):
+        if x%i == 0:
+            return False
+    else:
+            return True
+
+num = int(input("Enter an even number greater than 2 : \n"))
+for fnum in range(2,num):
+    for snum in range(2,fnum+1):
+        if testPrime(fnum) & testPrime(snum):
+            if ((fnum + snum) == num):
+                print(fnum," + ",snum," = ",num)
+    
+        
