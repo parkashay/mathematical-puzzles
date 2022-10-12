a09a2a8a20963c6fa4e799f40c5ebde4d2dc9b91
Prakash Poudel <72375446+GitZigsaw@users.noreply.github.com>
2022-10-12 11:03:53 +0545

    Goldbach's Conjecture

--- Goldbach'sConjecture.py
+++ Goldbach'sConjecture.py
@@ -4,15 +4,15 @@
 def testPrime(x):
     for i in range(2,x):
         if x%i == 0:
-            return False
-    else:                                   #beginner note : for-else and if else are two separate things in python. Here, for-else is used.
+            return False            #Beginner note : for-else and if-else are two separate things in python. Here, for-else is used.
+    else:
             return True
-
+c = 0
 num = int(input("Enter an even number greater than 2 : \n"))
 for fnum in range(2,num):
     for snum in range(2,fnum+1):
         if testPrime(fnum) & testPrime(snum):
             if ((fnum + snum) == num):
                 print(fnum," + ",snum," = ",num)
-    
-        
+                c = c+1
+print(c," pair(s) of prime numbers sum up to ",num)
